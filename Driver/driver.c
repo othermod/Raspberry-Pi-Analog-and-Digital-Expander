a7c8087508e330b90002ba524a861c475f6da540
othermod <22604540+othermod@users.noreply.github.com>
2022-10-22 11:26:59 -0400

    add drawing of battery overlay. does not update yet

--- Driver/driver.c
+++ Driver/driver.c
@@ -8,6 +8,11 @@
 #include <stdbool.h>
 #include <sys/ioctl.h>
 #include <linux/uinput.h>
+#include "imageGraphics.h"
+#include "imageLayer.h"
+#include <math.h>
+#include "bcm_host.h"
+#include <assert.h>
 int numberOfJoysticks = 2;
 
 #define SENSE_RESISTOR 100
@@ -32,6 +37,14 @@ int numberOfJoysticks = 2;
 #define BTN_16_BIT_READING ((I2C_DATA.buttonsPortD >> 0x03) & 1)
 
 #define I2C_ADDRESS 0x10
+
+// create colors ( format is: red, green, blue, opacity)
+static RGBA8_T clearColor = { 0,    0,    0,    0};
+static RGBA8_T green =      { 0,    255,  0,    255};
+static RGBA8_T red =        { 255,  0,    0,    255};
+static RGBA8_T orange =     { 255,  127,  0,    255};
+static RGBA8_T white =      { 255,  255,  255,  255};
+static RGBA8_T black =      { 0,    0,    0,    255};
 
 typedef struct {
   uint8_t buttonsPortB; // button status
@@ -171,6 +184,72 @@ void updateJoystick(int virtualGamepad) {
   }
 }
 
+void drawBattery(IMAGE_LAYER_T * batteryLayer) {
+  int batteryData = 19;
+  int  isCharging = 1;
+  IMAGE_T * image = & (batteryLayer -> image);
+  //clearImageRGB(image, & clearColor); //the image doesn't need to be erased because the same pixels are being used and colors are changing
+  RGBA8_T * batteryColor;
+  batteryColor = & green;
+  if (batteryData < 20) { // sets color depending on battery level
+    batteryColor = & orange;
+  }
+  if (batteryData < 10) {
+    batteryColor = & red;
+  }
+  // draw the battery outline and fill with color
+  imageBoxFilledRGB(image, 1, 0, 30, 14, & white);
+  imageBoxFilledRGB(image, 0, 4, 2, 10, & white);
+  imageBoxFilledRGB(image, 2, 1, 29, 13, & black);
+  imageBoxFilledRGB(image, 1, 5, 3, 9, & black);
+  imageBoxFilledRGB(image, 28 - batteryData / 4, 2, 28, 12, batteryColor);
+  if (isCharging) {
+    RGBA8_T * boltColor;
+    if (isCharging == 2) {
+      boltColor = & green;
+    } else {
+      boltColor = & white;
+    }
+    // draw the lightning bolt to show that the battery is charging
+    imageBoxFilledRGB(image, 15, 3, 16, 11, boltColor);
+    imageBoxFilledRGB(image, 12, 4, 14, 6, boltColor);
+    imageBoxFilledRGB(image, 10, 6, 11, 8, boltColor);
+    imageBoxFilledRGB(image, 7, 7, 9, 8, boltColor);
+    imageBoxFilledRGB(image, 17, 8, 19, 10, boltColor);
+    imageBoxFilledRGB(image, 20, 7, 21, 8, boltColor);
+    imageBoxFilledRGB(image, 22, 6, 24, 7, boltColor);
+    imageBoxFilledRGB(image, 14, 2, 17, 2, & black);
+    imageBoxFilledRGB(image, 17, 2, 17, 7, & black);
+    imageBoxFilledRGB(image, 18, 7, 19, 7, & black);
+    imageBoxFilledRGB(image, 19, 6, 21, 6, & black);
+    imageBoxFilledRGB(image, 21, 5, 25, 5, & black);
+    imageBoxFilledRGB(image, 25, 6, 25, 7, & black);
+    imageBoxFilledRGB(image, 24, 7, 24, 8, & black);
+    imageBoxFilledRGB(image, 23, 8, 22, 8, & black);
+    imageBoxFilledRGB(image, 22, 9, 20, 9, & black);
+    imageBoxFilledRGB(image, 20, 10, 19, 10, & black);
+    imageBoxFilledRGB(image, 19, 11, 17, 11, & black);
+    imageBoxFilledRGB(image, 17, 12, 14, 12, & black);
+    imageBoxFilledRGB(image, 14, 11, 14, 8, & black);
+    imageBoxFilledRGB(image, 14, 7, 12, 7, & black);
+    imageBoxFilledRGB(image, 12, 8, 10, 8, & black);
+    imageBoxFilledRGB(image, 10, 9, 6, 9, & black);
+    imageBoxFilledRGB(image, 6, 8, 6, 8, & black);
+    imageBoxFilledRGB(image, 6, 7, 7, 7, & black);
+    imageBoxFilledRGB(image, 7, 6, 9, 6, & black);
+    imageBoxFilledRGB(image, 9, 5, 11, 5, & black);
+    imageBoxFilledRGB(image, 11, 4, 12, 4, & black);
+    imageBoxFilledRGB(image, 12, 3, 14, 3, & black);
+  }
+  changeSourceAndUpdateImageLayer(batteryLayer);
+}
+
+void clearLayer(IMAGE_LAYER_T * layer) {
+  IMAGE_T * image = & (layer -> image);
+  clearImageRGB(image, & clearColor);
+  changeSourceAndUpdateImageLayer(layer);
+}
+
 int main(int argc, char * argv[]) {
   int I2CFile = openI2C(); // open I2C device
   if (ioctl(I2CFile, I2C_SLAVE, I2C_ADDRESS) < 0) { // initialize communication
@@ -178,16 +257,53 @@ int main(int argc, char * argv[]) {
     return 0;
   }
 
+  // set up the overlay
+  uint32_t displayNumber = 0;
+
+  bcm_host_init();
+
+  DISPMANX_DISPLAY_HANDLE_T display
+    = vc_dispmanx_display_open(displayNumber);
+  assert(display != 0);
+
+  DISPMANX_MODEINFO_T info;
+  int result = vc_dispmanx_display_get_info(display, & info);
+  assert(result == 0);
+
+  static int layer = 100000;
+
+  IMAGE_LAYER_T batteryLayer;
+  initImageLayer( & batteryLayer,
+    31, // battery image width
+    15, // battery image height
+    VC_IMAGE_RGBA16);
+  createResourceImageLayer( & batteryLayer, layer);
+
+
+  DISPMANX_UPDATE_HANDLE_T update = vc_dispmanx_update_start(0);
+  assert(update != 0);
+  int xOffset = info.width - 31;
+  int yOffset = 0;
+  addElementImageLayerOffset( & batteryLayer, xOffset, yOffset, display, update);
+
+  result = vc_dispmanx_update_submit_sync(update);
+  assert(result == 0);
+
+  drawBattery(& batteryLayer); // this is only done if something changes on the OSD
+
   int virtualGamepad = createUInputDevice(); // create uinput device
   updateButtons(virtualGamepad);
-  uint8_t previousPortB;
-  uint8_t previousPortD;
+
+  uint8_t previousPortB = 0;
+  uint8_t previousPortD = 0;
   while (1) {
+
     if (read(I2CFile, &I2C_DATA, sizeof(I2C_STRUCTURE)) != sizeof(I2C_STRUCTURE)) { // read the atmega
       printf("Controller is not detected on the I2C bus.\n");
       sleep(1);
     }
     else {
+
       bool updateUinput = 0;
       if ((previousPortB!=I2C_DATA.buttonsPortB) | (previousPortD!=I2C_DATA.buttonsPortD)) { // only update the buttons if something changed
         updateButtons(virtualGamepad);
